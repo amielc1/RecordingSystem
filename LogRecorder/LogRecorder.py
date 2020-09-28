1e78c25f3a9e0934b9dc6bcb62af4ce6cfc78fa4
Amiel Cohen <amielc1@gmail.com>
2020-09-28 21:54:14 +0300

    start work on #3

--- LogRecorder/LogRecorder.py
+++ LogRecorder/LogRecorder.py
@@ -1,13 +1,13 @@
 from base_recorder import recorder_base
 
-class log_recorder(recorder_base):
 
-    def copy_files(self,src_dir):
-        recorder_base.start_record()
-
+class LogRecorder(recorder_base):
 
     def __init__(self):
         recorder_base.__init__()
+
+    def copy_files(self, src_dir):
+        recorder_base.start_record()
 
     def start_record(self):
         recorder_base.start_record()
