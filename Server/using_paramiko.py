ebf88df85d14d550e3d179289cc58ce09e816fb2
Amiel Cohen <amielc1@gmail.com>
2020-10-05 15:27:02 +0300

    copy all files in directory, using paramiko. #10

--- Server/using_paramiko.py
+++ Server/using_paramiko.py
@@ -1,4 +1,16 @@
+import os
+from stat import S_ISDIR
+
 import paramiko
+
+
+def isdir(path):
+    try:
+        return S_ISDIR(sftp.stat(path).st_mode)
+    except IOError:
+        # Path does not exist, so by definition not a directory
+        return False
+
 
 paramiko.util.log_to_file("paramiko.log")
 
@@ -7,21 +19,20 @@ host, port = '127.0.0.1', 22
 transport = paramiko.Transport((host, port))
 
 # Auth
-username, password = "Me", ""
+username, password = "Me", "sale1sm6qt592"
 transport.connect(None, username, password)
 
 # Go!
 sftp = paramiko.SFTPClient.from_transport(transport)
 
-files = sftp.listdir()
-print(files)
-sftp.get('Downloads\AmiCo.txt', 'ami')
+log_dir = 'Downloads\log_files_dir'
+file_list = sftp.listdir(path=log_dir)
+print(file_list)
+for file in file_list:
+    print(os.path.join(log_dir, file))
+    print(os.path.join(os.getcwd(), file))
+    sftp.get(os.path.join(log_dir, file), os.path.join(os.getcwd(), file))
 
-# Download
-# filepath = "/etc/passwd"
-# localpath = "/home/remotepasswd"
-# sftp.get(filepath,localpath)
-#
 # # Upload
 # filepath = "/home/foo.jpg"
 # localpath = "/home/pony.jpg"
