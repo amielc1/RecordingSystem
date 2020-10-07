f1efc95d317a1fe0c530b21c058c245fb57b0939
Amiel Cohen <amielc1@gmail.com>
2020-10-07 15:49:40 +0300

    add LogRecorder test

--- tests/test_log_record.py
+++ tests/test_log_record.py
@@ -6,10 +6,13 @@ from LogRecorder import LogRecorder
 
 def test_copy_logs_pass():
     current_dir = Path().absolute()
-    log_recorder = LogRecorder(current_dir, "c:/tmp", 2, '*.*')
+    dest_file = "c:/tmp"
+    extn = '*.*'
+    log_recorder = LogRecorder(current_dir, dest_file, 2, extn)
     log_recorder.copy_logs()
-    for filename in glob.glob(current_dir):
-        print(filename)
+    lst = [filename for filename in glob.glob(os.path.join(dest_file, extn))]
+    for item in lst:
+        assert "LogRecorder" in item
 
 
 def test_cpy_files1():
