c867ee12ad388773b57e5b883f24bc74a764e9fc
amielc <amielc1@gmail.com>
2020-10-08 11:28:33 +0300

    Check Tests with  GithubActions

--- tests/test_log_record.py
+++ tests/test_log_record.py
@@ -2,12 +2,10 @@ import glob
 import os
 import shutil
 from pathlib import Path
-from LogRecorder import LogRecorder
+from LogRecorder.LogRecorder import LogRecorder
 
 
-# def arrange_test():
-#     pass
-#
+
 def test_copy_logs_pass():
     # arrange
     current_dir = Path().absolute()
@@ -22,22 +20,16 @@ def test_copy_logs_pass():
     matching = [s for s in glob.glob(os.path.join(dest_file, extn)) if "test_log_record.py" in s]
     assert len(matching) > 0
 
-
-#
-#
-# def test_copy_logs_by_extention_pass():
-#     # arrange
-#     current_dir = Path().absolute()
-#     dest_file = os.path.join(Path().absolute(), 'out')
-#     extn = '*.log'
-#     if os.path.isdir(dest_file):
-#         shutil.rmtree(dest_file)
-#     # act
-#     log_recorder = LogRecorder(current_dir, dest_file, 2, extn)
-#     log_recorder.copy_logs()
-#     # assert
-#     matching = [s for s in glob.glob(os.path.join(dest_file, extn)) if "test_log_record.py" in s]
-#     assert len(matching) == 0
-
-def test_pass():
-    assert 1 == 1
+def test_copy_logs_by_extention_pass():
+    # arrange
+    current_dir = Path().absolute()
+    dest_file = os.path.join(Path().absolute(), 'out')
+    extn = '*.log'
+    if os.path.isdir(dest_file):
+        shutil.rmtree(dest_file)
+    # act
+    log_recorder = LogRecorder(current_dir, dest_file, 2, extn)
+    log_recorder.copy_logs()
+    # assert
+    matching = [s for s in glob.glob(os.path.join(dest_file, extn)) if "test_log_record.py" in s]
+    assert len(matching) == 0
