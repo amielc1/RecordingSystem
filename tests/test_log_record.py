fdde2f4c81bc31265006585a534b205762e1cf15
Amiel Cohen <amielc1@gmail.com>
2020-10-10 23:32:35 +0300

    test and req

--- tests/test_log_record.py
+++ tests/test_log_record.py
@@ -19,14 +19,33 @@ def test_logRecorder_copy_logs_pass(tmpdir_factory):
     matching = [s for s in glob.glob(os.path.join(dst, extn)) if my_file_name in s]
     assert len(matching) > 0
 
-# def test_logRecorder_copy_logs_by_extention_pass(tmpdir_factory):
-#     # arrange
-#     src = Path().absolute()
-#     dst = tmpdir_factory.mktemp("dst")
-#     extn = '*.log'
-#     # act
-#     log_recorder = LogRecorder(src, dst, 2, extn)
-#     log_recorder.copy_logs()
-#     # assert
-#     matching = [s for s in glob.glob(os.path.join(dst, extn)) if "test_log_record.py" in s]
-#     assert len(matching) == 0
+
+def test_logRecorder_copy_logs_by_extention_pass(tmpdir_factory):
+    # arrange
+    src = tmpdir_factory.mktemp("src")
+    dst = tmpdir_factory.mktemp("dst")
+    extn = '*.log'
+    my_file_name = 'amico.py'
+    p = src.join(my_file_name)
+    p.write("content")
+    # act
+    log_recorder = LogRecorder(src, dst, 2, extn)
+    log_recorder.copy_logs()
+    # assert
+    matching = [s for s in glob.glob(os.path.join(dst, extn)) if my_file_name in s]
+    assert len(matching) == 0
+
+
+def write_to_log():
+    src = tmpdir_factory.mktemp("src")
+
+
+def test_logRecorder_start_record(tmpdir_factory):
+    # the recorder should to collect files periodically.
+    # arrange
+    src = tmpdir_factory.mktemp("src")
+
+    # act
+    log_recorder = LogRecorder(src, dst, 2, extn)
+    log_recorder.start()
+    # assert
